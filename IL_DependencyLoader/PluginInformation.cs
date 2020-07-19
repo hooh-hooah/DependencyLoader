182f594c10929adaa183d944f6f9d36f9e87b8fe
hooh_hooah <hooh_hooah@protonmail.com>
2020-07-19 22:07:34 +0000

    Increased version number

--- IL_DependencyLoader/PluginInformation.cs
+++ IL_DependencyLoader/PluginInformation.cs
@@ -2,7 +2,7 @@ namespace IL_DependencyLoader
 {
     public static class PluginInformation
     {
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
         public const string ReadableName = "DependencyLoader";
         public const string Name = "deploader";
         public const string CommonPrefix = "com.hooh";
